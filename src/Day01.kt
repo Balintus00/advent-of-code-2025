b39e45a25a3930640d71a021894b9aa9dc4e3af5
Balintus00 <60960681+Balintus00@users.noreply.github.com>
2025-12-01 19:22:01 +0100

    feat: finish day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,35 +1,76 @@
 fun main() {
-    part1()
-}
-
-private fun part1() {
     val input = readInput("Day01")
 
-    var zeroPositionCounter = 0
+    part1(input)
+    part2(input)
+}
 
+private fun part1(input: List<String>) {
+    var zeroEndPositionCounter = 0
+
+    executeSteps(
+        input = input,
+        onStepEndedOnZero = { zeroEndPositionCounter += 1 }
+    )
+
+    println("Part 1 result is $zeroEndPositionCounter")
+}
+
+private fun executeSteps(
+    input: List<String>,
+    onStepEndedOnZero: (() -> Unit)? = null,
+    onStepCrossedZero: (() -> Unit)? = null,
+) {
     var currentPosition = 50
 
     input.forEach { step ->
         val stepSize = step.removeRange(0, 1).toInt()
+        val isStartingFromZero = currentPosition == 0
 
         currentPosition = when (step.first()) {
             'L' -> currentPosition - stepSize
             'R' -> currentPosition + stepSize
-            else -> throw IllegalArgumentException("Unexpected turn direction input: ${step.first()}")
-        }
-
-        while (currentPosition < 0) {
-            currentPosition += 100
-        }
-
-        if (currentPosition > 99) {
-            currentPosition %= 100
+            else -> throw IllegalArgumentException("Unexpected direction input: ${step.first()}")
         }
 
         if (currentPosition == 0) {
-            zeroPositionCounter += 1
+            onStepCrossedZero?.invoke()
+        }
+
+        var hasDecrementCrossingHappenedBefore = false
+        while (currentPosition < 0) {
+            currentPosition += 100
+
+            if (hasDecrementCrossingHappenedBefore || !isStartingFromZero) {
+                onStepCrossedZero?.invoke()
+            }
+
+            hasDecrementCrossingHappenedBefore = true
+        }
+
+        while (currentPosition > 99) {
+            currentPosition -= 100
+
+            onStepCrossedZero?.invoke()
+        }
+
+        if (currentPosition == 0) {
+            onStepEndedOnZero?.invoke()
+
+            if (hasDecrementCrossingHappenedBefore) {
+                onStepCrossedZero?.invoke()
+            }
         }
     }
+}
 
-    println("Part 1 result is $zeroPositionCounter")
+private fun part2(input: List<String>) {
+    var zeroClickCounter = 0
+
+    executeSteps(
+        input = input,
+        onStepCrossedZero = { zeroClickCounter += 1 }
+    )
+
+    println("Part 2 result is $zeroClickCounter")
 }
